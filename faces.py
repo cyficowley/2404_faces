3faca48c37ed2f8484700da9e6a0654f4e467e60
Cyrus Cowley <cyficowley@gmail.com>
2018-10-27 21:38:56 -0700

    auto adding of faces, but manually choose which face

--- faces.py
+++ faces.py
@@ -4,75 +4,99 @@ import os
 import operator
 
 # Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
 
-people = {
-  "cyrus":[],
-  "nick":[],
-  "surya":[]
-}
+class faces:
 
+  def __init__(self):
+    self.people = {}
 
-for root, dirs, files in os.walk("faces"):
-  for person in dirs:
-    for root, dirs, files in os.walk(os.path.join("faces", person)):
-      for image in files:
-        if image.endswith(".jpg"):
-          path = os.path.join("faces", person, image)
-          encoding = face_recognition.face_encodings(face_recognition.load_image_file(path))[0]
-          if(person in people):
-            people[person].append(encoding)
-          else:
-            people[person] = [encoding]
+    for root, dirs, files in os.walk("faces"):
+      for person in dirs:
+        for root, dirs, files in os.walk(os.path.join("faces", person)):
+          for image in files:
+            if image.endswith(".jpg"):
+              path = os.path.join("faces", person, image)
+              encoding = face_recognition.face_encodings(face_recognition.load_image_file(path))[0]
+              if(person in self.people):
+                self.people[person].append(encoding)
+              else:
+                self.people[person] = [encoding]
 
 
-known_encodings = []
-names = []
+    self.known_encodings = []
+    self.names = []
 
-for name, encodings_list in people.items():
-  names += [name] * len(encodings_list)
-  print(encodings_list)
-  known_encodings += encodings_list
-
+    for name, encodings_list in self.people.items():
+      self.names += [name] * len(encodings_list)
+      self.known_encodings += encodings_list
 
 
-while True:
-  ret, frame = video_capture.read()
+  def main_loop(self):
+    video_capture = cv2.VideoCapture(0)
 
-  # Resize frame of video to 1/4 size for faster face recognition processing
-  small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    while True:
+      ret, frame = video_capture.read()
 
-  # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-  rgb_small_frame = small_frame[:, :, ::-1]
+      # Resize frame of video to 1/4 size for faster face recognition processing
+      small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-  face_locations = face_recognition.face_locations(rgb_small_frame)
-  face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+      # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+      rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
 
-  face_names = []
+      face_locations = face_recognition.face_locations(rgb_small_frame)
+      face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-  all_matches = []
-  for face_encoding in face_encodings:
-    # See if the face is a match for the known face(s)
-    matches = face_recognition.compare_faces(known_encodings, face_encoding)
-    
-    totals = {name:0 for name in people.keys()}
+      face_names = []
 
-    # print(matches)
+      all_matches = []
+      for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(self.known_encodings, face_encoding)
+        
+        totals = {name:0 for name in self.people.keys()}
 
-    for i in range(len(matches)):
-      if(matches[i]):
-        totals[names[i]] += 1
+        for i in range(len(matches)):
+          if(matches[i]):
+            totals[self.names[i]] += 1
 
-    max_person = max(totals.items(), key=operator.itemgetter(1))
-    if(max_person[1] != 0):
-      all_matches.append(max_person[0])
-    else:
-      print("unknown face")
+        max_person = max(totals.items(), key=operator.itemgetter(1))
+        if(max_person[1] != 0):
+          all_matches.append(max_person[0])
+        else:
+          if(len(face_encodings) == 1):
+            self._add_new_face("surya", frame)
+          
       
-  
-  if(face_locations):
-    print(all_matches)
+      if(face_locations):
+        print(all_matches)
 
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+
+  def _add_new_face(self, name, face_image):
+    print("ADDING NAME {}".format(name))
+    if(not os.path.isdir(os.path.join("faces", name))):
+      os.mkdir(os.path.join("faces", name))
+    
+    total_files = 0
+    for each in os.listdir(os.path.join("faces", name)):
+      total_files += 1
+    cv2.imwrite(os.path.join("faces", name,"img_{}.jpg".format(total_files)), face_image)
+    
+    encoding = face_recognition.face_encodings(face_image)[0]
+    
+    self.known_encodings.append(encoding)
+    self.names.append(name)
+    if(name in self.people):
+      self.people[name].append(encoding)
+    else:
+      self.people[name] = [encoding]
+
+
+
+if(__name__=="__main__"):
+  face = faces()
+  face.main_loop()
