7c5eb4807f792b0dbd4f4d8f1e6ba1e55ac1fc02
Cyrus Cowley <cyficowley@gmail.com>
2019-01-15 18:48:21 -0800

    finally set up bluetooth with that stupid ihome thing it was so hard

--- text2speech.py
+++ text2speech.py
@@ -1,7 +1,12 @@
 from os import system
 
 class Text2Speech:
+    def __init__(self):
+        self.bluetooth_device = 'FC:58:FA:2A:15:98'
 
     def say(self, msg):
-        message = 'flite -voice slt -t "{}"'.format(msg)
-        system(message)
+        create_wave = 'flite -o neat.wav -voice slt -t "{}"'.format(msg)
+        system(create_wave)
+        say_wave = 'aplay -D bluealsa:HCI=hci0,DEV={},PROFILE=a2dp neat.wav'.format(self.bluetooth_device)
+        system(say_wave)
+        
